7faa1d289b0f387f3253f60b6169da8cfb0c5fb9
michael.staehler <michael.staehler@opitz-consulting.com>
2013-05-06 14:13:23 +0200

    added comment

--- src/main/java/com/opitzconsulting/springdata/jpa/ApplicationConfig.java
+++ src/main/java/com/opitzconsulting/springdata/jpa/ApplicationConfig.java
@@ -6,10 +6,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Application specific Java configuration.
+ * <p/>
+ * <p>NOTE: There is not auditing java config available yet.
+ * @see <a href="https://jira.springsource.org/browse/DATAJPA-265">https://jira.springsource.org/browse/DATAJPA-265</a> for details.
  */
 @Configuration
 @EnableJpaRepositories
 @Import(InfrastructureConfig.class)
 public class ApplicationConfig {
+
 
 }
