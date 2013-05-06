3185451c07f3601ff3538016252f7760f55e4198
michael.staehler <michael.staehler@opitz-consulting.com>
2013-05-06 14:41:49 +0200

    added comment

--- src/main/java/com/opitzconsulting/springdata/jpa/auditing/AuditorAwareImpl.java
+++ src/main/java/com/opitzconsulting/springdata/jpa/auditing/AuditorAwareImpl.java
@@ -5,6 +5,11 @@ import com.opitzconsulting.springdata.jpa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.AuditorAware;
 
+/**
+ * Application specific implementation to return the current user.
+ *
+ * <p>NOTE: This is only a dummy implementation. Usually you would use a security framework to return thos.</p>
+ */
 public class AuditorAwareImpl implements AuditorAware<User> {
 
     public static final String USERNAME_FOR_SYSTEM_USER = "Fred";
