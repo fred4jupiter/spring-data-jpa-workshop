d3ecc2ea3a8f0ca0bb18006f99bf0be9a2282f10
michael.staehler <michael.staehler@opitz-consulting.com>
2013-05-08 13:45:55 +0200

    update tests

--- src/test/java/com/opitzconsulting/springdata/jpa/QueryDslJpaTest.java
+++ src/test/java/com/opitzconsulting/springdata/jpa/QueryDslJpaTest.java
@@ -9,6 +9,9 @@ import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasItem;
 import static org.junit.Assert.*;
 
 public class QueryDslJpaTest extends AbstractTestingBase {
@@ -17,24 +20,24 @@ public class QueryDslJpaTest extends AbstractTestingBase {
     private CustomerRepository customerRepository;
 
     @Test
-    public void findCustomerWithBirthdayOn_UsingPlaingQueryDSLWithJPA() {
+    public void findCustomerWithBirthdayOn_using_QueryDSL_with_JPA() {
         final Customer customer = createAndSaveCustomer();
 
         JPQLQuery query = new JPAQuery(entityManager);
         QCustomer qCustomer = QCustomer.customer;
 
-        Customer michael = query.from(qCustomer).where(qCustomer.birthday.eq(customer.getBirthday())).uniqueResult(qCustomer);
-        assertEquals(customer, michael);
+        List<Customer> customerList = query.from(qCustomer).where(qCustomer.birthday.eq(customer.getBirthday())).list(qCustomer);
+        assertThat(customerList, hasItem(customer));
     }
 
     @Test
-    public void findCustomerWithBirthdayOn_UsingSpringDataJpa() {
+    public void findCustomerWithBirthdayOn_using_QueryDSL_with_SpringDataJpa() {
         final Customer customer = createAndSaveCustomer();
 
         QCustomer qCustomer = QCustomer.customer;
 
-        Customer michael = customerRepository.findOne(qCustomer.birthday.eq(customer.getBirthday()));
-        assertEquals(customer, michael);
+        Iterable<Customer> customers = customerRepository.findAll(qCustomer.birthday.eq(customer.getBirthday()));
+        assertThat(customers, hasItem(customer));
     }
 
     private Customer createAndSaveCustomer() {
